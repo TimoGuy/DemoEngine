0e51fe133b29089319471afa0d896e833826977e
Hawsoo <timoguy99004@gmail.com>
2021-12-24 12:27:13 -0800

    Makes release build config show no console

--- TestGUI/src/Main.cpp
+++ TestGUI/src/Main.cpp
@@ -1,7 +1,12 @@
 #include "mainloop/MainLoop.h"
 
-
+// NOTE: below is the difference between debug/checked and release.
+// With release, we want to disable the console, so subsystem is set to Windows.
+#ifdef _DEVELOP
 int main()
+#else
+int __stdcall WinMain(void*, void*, char* cmdLine, int)
+#endif
 {
 	MainLoop::getInstance().initialize();
 	MainLoop::getInstance().run();
