6e052715f602e585da6909a009957f9bda74c3c5
Hawsoo <timoguy99004@gmail.com>
2022-01-13 16:00:41 -0800

    Updates recommended specs message

--- TestGUI/src/Main.cpp
+++ TestGUI/src/Main.cpp
@@ -3,11 +3,18 @@
 //
 // ABOUT THIS PROGRAM
 // 
-// Specs:
+// Recommended Specs:
 //		MINIMUM:
-//			OpenGL 4.5 support (Minimum GTX 740m)
+//			OpenGL 4.5 support (Minimum GTX 600 series and Radeon HD 7000 series)
+//			https://www.reddit.com/r/opengl/comments/edw1sh/graphics_card_that_supports_opengl_45/
+// 
+//			Intel® HD Graphics 520/530 or Intel® Iris® Plus Graphics 540 / 550 / 580
+//			https://www.intel.com/content/www/us/en/support/articles/000005524/graphics.html
+// 
 //		RECOMMENDED:
 //			GTX 1060 or higher
+//			Seems like for AMD side the Radeon RX 5600XT is higher than the 1060 and the Radeon RX580 is lower
+//			https://www.videocardbenchmark.net/common_gpus.html
 // 
 // Developers:
 //		Timothy Bennett
