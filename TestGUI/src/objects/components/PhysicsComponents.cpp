8082e9a30d9b48a3ca471b51f6d33fa17c81622f
TimoGuy <timoguy99004@gmail.com>
2022-02-12 19:24:06 -0800

    Bugfix for ground sliding weirdness

--- TestGUI/src/objects/components/PhysicsComponents.cpp
+++ TestGUI/src/objects/components/PhysicsComponents.cpp
@@ -418,8 +418,7 @@ void PlayerPhysics::physicsUpdate()
 					slidingVectorXZ.x,
 					fallVelocity,
 					slidingVectorXZ.y
-				)
-				+ PhysicsUtils::toPxVec3(ceilingHitNormal * 0.1f);
+				);
 			controller->move(
 				offsetVector,
 				0.01f,
