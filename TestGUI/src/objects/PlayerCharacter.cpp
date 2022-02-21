860fc27f9b690eb56ab69cc69f9a335f322d1e04
TimoGuy <timoguy99004@gmail.com>
2022-02-20 21:16:43 -0800

    Extracts playerphysics outside of physicscomponents.h/.cpp

--- TestGUI/src/objects/PlayerCharacter.cpp
+++ TestGUI/src/objects/PlayerCharacter.cpp
@@ -23,6 +23,407 @@
 #define REMAP(value, istart, istop, ostart, ostop) ((value) - (istart)) / ((istop) - (istart)) * ((ostop) - (ostart)) + (ostart)
 
 
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ROPESIMULATION
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+void RopeSimulation::initializePoints(const std::vector<glm::vec3>& points)
+{
+	RopeSimulation::points = RopeSimulation::prevPoints = points;
+
+	size_t counter = 0;
+	for (size_t i = 0; i < points.size() - 1; i++)
+	{
+		distances.push_back(glm::length(points[i] - points[i + 1]));
+	}
+}
+
+void RopeSimulation::setPointPosition(size_t index, float trickleRate, const glm::vec3& position)
+{
+	glm::vec3 deltaMovement = position - points[index];
+	points[index] = position;
+
+	// NOTE: this is for alleviating glitchyness when point moves too much.
+	// This code propogates the movement from this new set operation to the other points of the rope
+	for (size_t i = 0; i < prevPoints.size(); i++)
+	{
+		prevPoints[i] += deltaMovement * glm::pow(trickleRate, (float)(i + 1));		// NOTE: I tried doing size-i, bc I thought that was more correct, but no, just doing i+1 for the exponent is correct -Timo 01-17-2022
+	}
+}
+
+void RopeSimulation::simulateRope(float gravityMultiplier)
+{
+	const float deltaTime = MainLoop::getInstance().deltaTime;
+
+	//
+	// Part 1: cycle thru all points (except #0 is locked, so skip that one), and update their positions
+	//
+	for (size_t i = 1; i < points.size(); i++)
+	{
+		glm::vec3 savedPoint = points[i];
+		points[i] += (points[i] - prevPoints[i]) + glm::vec3(0, -9.8f * gravityMultiplier * deltaTime * deltaTime, 0);		// TODO: figure out why the gravity term requires deltaTime * deltaTime instead of regular stuff huh
+
+		// Limit to 45 degrees
+		if (limitTo45degrees)
+		{
+			const float sin45deg = 0.707106781f;
+			points[i].y = std::min(points[i].y, points[i - 1].y - distances[i - 1] * sin45deg);
+		}
+
+		prevPoints[i] = savedPoint;
+	}
+
+	//
+	// Part 2: solve x times to try to get the distance correct again!
+	//
+	const int numIterations = 10;
+	for (int i = 0; i < numIterations; i++)
+	{
+		for (size_t j = 0; j < distances.size(); j++)
+		{
+			//if (glm::length(points[j] - points[j + 1]) < distances[j])
+			//	continue;
+
+			glm::vec3 midpoint = (points[j] + points[j + 1]) / 2.0f;
+			glm::vec3 direction = glm::normalize(points[j] - points[j + 1]);
+			if (j > 0)
+				points[j] = midpoint + direction * distances[j] / 2.0f;
+			points[j + 1] = midpoint - direction * distances[j] / 2.0f;
+		}
+		for (int j = (int)distances.size() - 1; j >= 0; j--)
+		{
+
+			// NOTE: ignore dumb warnings
+			glm::vec3 midpoint = (points[j] + points[j + 1]) / 2.0f;
+			glm::vec3 direction = glm::normalize(points[j] - points[j + 1]);
+			if (j > 0)
+				points[j] = midpoint + direction * distances[j] / 2.0f;
+			points[j + 1] = midpoint - direction * distances[j] / 2.0f;
+		}
+	}
+}
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+// PLAYERPHYSICS
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+float playerCapsuleControllerRadius = 1.0f;
+PlayerPhysics::PlayerPhysics(BaseObject* bo) : PhysicsComponent(bo)
+{
+	controller =
+		PhysicsUtils::createCapsuleController(
+			MainLoop::getInstance().physicsControllerManager,
+			MainLoop::getInstance().defaultPhysicsMaterial,
+			physx::PxExtendedVec3(0.0f, 100.0f, 0.0f),
+			playerCapsuleControllerRadius,
+			4.5f,
+			this,		// PxUserControllerHitReport
+			this//,		// PxBehaviorCallbackWhatevs
+			//0.0f		// Undo the slope climbing stuff		@TODO: fix this!!!!!!
+		);
+
+	GameState::getInstance().playerActorPointer = controller->getActor();
+	body = controller->getActor();
+
+	physx::PxFilterData filterData;
+	filterData.word0 = (physx::PxU32)PhysicsUtils::Word0Tags::ENTITY;
+
+	std::vector<physx::PxShape*> shapes;
+	shapes.resize(body->getNbShapes());
+	int numShapes = body->getShapes(&shapes[0], shapes.size());
+	for (size_t i = 0; i < numShapes; i++)
+		shapes[i]->setQueryFilterData(filterData);
+}
+
+PlayerPhysics::~PlayerPhysics()
+{
+	controller->release();
+}
+
+void PlayerPhysics::lockVelocity(bool yAlso)
+{
+	if (yAlso)
+		velocity = physx::PxVec3(0);
+	else
+	{
+		velocity.x = 0;
+		velocity.z = 0;
+	}
+}
+
+
+#ifdef _DEVELOP
+bool playerPhysicsOverrideVelocityInheritanceBcOfImguiInterference = true;		// NOTE: just in case first frame may get messed up, this flag is set to true at first.
+#endif
+
+
+void PlayerPhysics::physicsUpdate()
+{
+	//
+	// Add gravity (or sliding gravity if sliding)
+	//
+	velocity.y -= 4.9f * MainLoop::getInstance().physicsDeltaTime;
+
+	physx::PxVec3 cookedVelocity = velocity;
+
+	// Add force to go down stairs
+	if (isGrounded && !isSliding)
+		cookedVelocity.y = -controller->getStepOffset();
+
+	//
+	// Do the deed
+	//
+	physx::PxControllerCollisionFlags collisionFlags = controller->move(cookedVelocity, 0.01f, MainLoop::getInstance().physicsDeltaTime, NULL, NULL);
+	bool prevIsSliding = isSliding;
+	isGrounded = false;
+	isSliding = false;
+	isSlidingCeiling = false;
+	isSandwiching = false;
+	hasValidStandingOnAngularVelocityY = false;
+
+	// Check if collision on top and bottom
+	if (collisionFlags & physx::PxControllerCollisionFlag::eCOLLISION_DOWN &&
+		collisionFlags & physx::PxControllerCollisionFlag::eCOLLISION_UP)
+	{
+		// Move away at the average of the two normals
+		glm::vec3 avgHitNormal = (groundHitNormal + ceilingHitNormal) / 2.0f;
+		avgHitNormal.y = 0.0f;
+		avgHitNormal = glm::normalize(avgHitNormal);
+
+		controller->move(
+			PhysicsUtils::toPxVec3(avgHitNormal * 0.1f),
+			0.01f,
+			MainLoop::getInstance().physicsDeltaTime,
+			NULL,
+			NULL
+		);
+	}
+
+	// Collision on bottom
+	if (collisionFlags & physx::PxControllerCollisionFlag::eCOLLISION_DOWN)
+	{
+		isGrounded = true;
+		bool isFlatGround = (glm::dot(groundHitNormal, glm::vec3(0, 1, 0)) > 0.69465837f);
+
+		// Try to retrieve grounded info (don't assign to isFlatGround if currently sliding)
+		physx::PxRaycastBuffer hitInfo;
+		constexpr float padding = 0.05f;
+		if (PhysicsUtils::raycast(
+			PhysicsUtils::toPxVec3(controller->getFootPosition()) + physx::PxVec3(0, padding, 0),
+			physx::PxVec3(0, -1, 0),
+			controller->getStepOffset() + padding + padding,
+			hitInfo)
+			&& !prevIsSliding)
+			isFlatGround |= (glm::dot(PhysicsUtils::toGLMVec3(hitInfo.block.normal), glm::vec3(0, 1, 0)) > 0.69465837f);
+
+		if (isFlatGround)
+		{
+			velocity.y = 0.0f;		// Remove gravity
+
+			if (hitInfo.hasBlock && (hitInfo.block.actor->getType() & physx::PxActorType::eRIGID_DYNAMIC))		// @TODO: @REFACTOR: attach the actor via the onshapecollide() callback instead of a downwards raycast (only use as fallback)
+			{
+				physx::PxRigidDynamic* body = (physx::PxRigidDynamic*)hitInfo.block.actor;
+				standingOnAngularVelocityYRadians = body->getAngularVelocity().y;
+				hasValidStandingOnAngularVelocityY = true;
+			}
+		}
+		else if (velocity.y < 0.0f)
+		{
+			isSliding = true;		// Slide down!
+
+			// Slide down normal going downhill (NOTE: This algorithm is different from the slide up normal uphill counterpart. This is bc the way the capsule controller treats these types of collisions is different. This version pushes the controller horizontally away from the wall, the uphill version redirects the upwards velocity along the normal of the wall  -Timo)
+			float fallVelocity = velocity.y - offsetFootMovedReconstructed.y;
+			const glm::vec3 upXnormal = glm::cross(glm::vec3(0, 1, 0), groundHitNormal);
+			const glm::vec3 uxnXnormal = glm::normalize(glm::cross(upXnormal, groundHitNormal));
+			const glm::vec2 slidingVectorXZ = glm::vec2(uxnXnormal.x, uxnXnormal.z) * fallVelocity / uxnXnormal.y;
+
+			glm::vec2 pushOffVelocity(0.0f);
+			glm::vec2 flatVelocity(velocity.x, velocity.z);
+			if (glm::length2(flatVelocity) > 0.001f)
+				pushOffVelocity = glm::min(glm::dot(glm::normalize(glm::vec2(groundHitNormal.x, groundHitNormal.z)), glm::normalize(flatVelocity)), 0.0f) * flatVelocity;
+
+			physx::PxVec3 offsetVector =
+				physx::PxVec3(
+					slidingVectorXZ.x,
+					fallVelocity,
+					slidingVectorXZ.y
+				);
+			controller->move(
+				offsetVector,
+				0.01f,
+				MainLoop::getInstance().physicsDeltaTime,
+				NULL,
+				NULL
+			);
+		}
+	}
+
+	// Collision on sides
+	if (collisionFlags & physx::PxControllerCollisionFlag::eCOLLISION_SIDES)
+	{
+		//std::cout << "\tSide Collision";
+	}
+
+	// Collision on head
+	if (velocity.y > 0.0f && collisionFlags & physx::PxControllerCollisionFlag::eCOLLISION_UP)
+	{
+		if (glm::dot(ceilingHitNormal, glm::vec3(0, -1, 0)) > 0.69465837f)
+		{
+			velocity.y = 0.0f;		// Hit your head on the ceiling
+		}
+		else if (velocity.y > 0.0f)
+		{
+			isSlidingCeiling = true;
+
+			// Slide up normal going uphill (https://www.youtube.com/watch?v=GI5LAbP5slE)
+			float riseVelocity = velocity.y - offsetHeadMovedReconstructed.y;
+			const glm::vec3 slopeDirection = glm::vec3(0, -1, 0) - ceilingHitNormal * glm::dot(glm::vec3(0, -1, 0), ceilingHitNormal);
+			const glm::vec3 moveDirection = -riseVelocity * slopeDirection;
+
+			physx::PxVec3 offsetVector = PhysicsUtils::toPxVec3(moveDirection);
+			controller->move(
+				offsetVector,
+				0.01f,
+				MainLoop::getInstance().physicsDeltaTime,
+				NULL,
+				NULL
+			);
+			playerPhysicsOverrideVelocityInheritanceBcOfImguiInterference = true;		// NOTE: Prevent single-tick bounce off when ceiling is right above you. Since isGrounded's state changes at the same time you do this, then you fly away real quick  -Timo
+		}
+	}
+
+	//
+	// Apply transform
+	//
+	physx::PxExtendedVec3 pos = controller->getPosition();
+	glm::mat4 trans = baseObject->getTransform();
+	trans[3] = glm::vec4(pos.x, pos.y, pos.z, 1.0f);
+	baseObject->INTERNALsubmitPhysicsCalculation(trans);
+
+	//
+	// Set the velocity when leaving or landing the ground
+	//
+#ifdef _DEVELOP
+	if (playerPhysicsOverrideVelocityInheritanceBcOfImguiInterference)		// BLOCKING FLAG
+		playerPhysicsOverrideVelocityInheritanceBcOfImguiInterference = false;
+	else
+#endif
+		if (prevIsGrounded != isGrounded)
+		{
+			// Set the velocity!
+			velocity = PhysicsUtils::toPxVec3(pos) - prevPositionWhileGrounded;
+
+			if (isGrounded)
+			{
+				// Landing on the ground
+				glm::vec2 flatVelocity(velocity.x, velocity.z);
+				float newRunSpeed = glm::length(flatVelocity);
+
+				if (newRunSpeed > 0.01f)
+				{
+					glm::vec2 idealFacingDirection = glm::normalize(flatVelocity);
+					float facingDotIdeal = glm::dot(((PlayerCharacter*)baseObject)->facingDirection, idealFacingDirection);
+					if (facingDotIdeal < 0.0f)
+					{
+						idealFacingDirection *= -1.0f;
+						newRunSpeed *= -1.0f;
+					}
+
+					((PlayerCharacter*)baseObject)->facingDirection = idealFacingDirection;		// ; TODO: figger out turning speed problems!!! (MAYBE: just have there be an exponential function on turn speed. If moving a certain speed, make turn speed move towards 0 like asymptote)
+					((PlayerCharacter*)baseObject)->currentRunSpeed = newRunSpeed;
+				}
+			}
+			else
+				// Leaving the ground
+				velocity.y = glm::max(velocity.y, 0.0f);
+		}
+	prevPositionWhileGrounded = PhysicsUtils::toPxVec3(pos);		// Use this to create the velocity
+	prevIsGrounded = isGrounded;
+
+	// @NOTE: Make this not show up in the release game!!!! @DEBUG only... but it's still kinda glitchy so we need it.
+	//#ifdef _DEVELOP
+		// Set a save point for the player if press pause!
+	static physx::PxExtendedVec3 savedPlayerResetPoint{ 0, 4, 0 };
+	if (InputManager::getInstance().pausePressed)
+	{
+		savedPlayerResetPoint = controller->getFootPosition();
+		std::cout << "::PLAYER:: set reset point to { " << savedPlayerResetPoint.x << ", " << savedPlayerResetPoint.y << ", " << savedPlayerResetPoint.z << " }" << std::endl;
+	}
+
+	// Reset position of player to 0, 0, 0!
+	if (InputManager::getInstance().inventoryPressed)
+	{
+		controller->setFootPosition(savedPlayerResetPoint);
+		velocity = { 0, 0, 0 };
+		((PlayerCharacter*)baseObject)->currentRunSpeed = 0.0f;
+	}
+	//#endif
+}
+
+void PlayerPhysics::propagateNewTransform(const glm::mat4& newTransform)
+{
+	glm::vec3 pos = PhysicsUtils::getPosition(newTransform);
+	controller->setPosition(physx::PxExtendedVec3(pos.x, pos.y, pos.z));
+
+#ifdef _DEVELOP
+	playerPhysicsOverrideVelocityInheritanceBcOfImguiInterference = true;
+#endif
+}
+
+physx::PxTransform PlayerPhysics::getGlobalPose()
+{
+	physx::PxExtendedVec3 pos = controller->getPosition();
+	return PhysicsUtils::createTransform(glm::vec3(pos.x, pos.y, pos.z));
+}
+
+void PlayerPhysics::onShapeHit(const physx::PxControllerShapeHit& hit)
+{
+	if (hit.worldNormal.y >= 0.0f)
+	{
+		groundHitNormal = glm::vec3(hit.worldNormal.x, hit.worldNormal.y, hit.worldNormal.z);
+		offsetFootMovedReconstructed =
+			PhysicsUtils::toPxExtendedVec3(
+				(hit.worldPos
+					+ PhysicsUtils::toPxExtendedVec3(hit.worldNormal * playerCapsuleControllerRadius)
+					+ physx::PxExtendedVec3(0, -playerCapsuleControllerRadius, 0))
+				- controller->getFootPosition()
+			);		// NOTE: this is probably getting truncated, the data types. for some reason pxextendedvec3-pxextendedvec3=pxvec3???
+	}
+	else
+	{
+		ceilingHitNormal = glm::vec3(hit.worldNormal.x, hit.worldNormal.y, hit.worldNormal.z);
+		offsetHeadMovedReconstructed =
+			PhysicsUtils::toPxExtendedVec3(
+				(hit.worldPos
+					+ PhysicsUtils::toPxExtendedVec3(hit.worldNormal * playerCapsuleControllerRadius)
+					+ physx::PxExtendedVec3(0, -controller->getHeight() - playerCapsuleControllerRadius, 0))
+				- controller->getFootPosition()
+			);		// NOTE: this is probably getting truncated, the data types. for some reason pxextendedvec3-pxextendedvec3=pxvec3???
+	}
+}
+
+void PlayerPhysics::onControllerHit(const physx::PxControllersHit& hit) { PX_UNUSED(hit); }
+void PlayerPhysics::onObstacleHit(const physx::PxControllerObstacleHit& hit) { PX_UNUSED(hit); }
+
+// https://github.com/NVIDIAGameWorks/PhysX-3.4/blob/master/PhysX_3.4/Samples/SampleBridges/SampleBridgesCCT.cpp
+physx::PxControllerBehaviorFlags PlayerPhysics::getBehaviorFlags(const physx::PxShape& shape, const physx::PxActor& actor)
+{
+	return physx::PxControllerBehaviorFlag::eCCT_CAN_RIDE_ON_OBJECT;		// NOTE: the physx-provided sliding feature doesn't work so well eh.
+}
+
+physx::PxControllerBehaviorFlags PlayerPhysics::getBehaviorFlags(const physx::PxController&)
+{
+	return physx::PxControllerBehaviorFlags(0);
+}
+
+physx::PxControllerBehaviorFlags PlayerPhysics::getBehaviorFlags(const physx::PxObstacle&)
+{
+	return physx::PxControllerBehaviorFlags(0);
+}
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+// PLAYERCHARACTER
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------
 PlayerCharacter::PlayerCharacter() : bottleModelMatrix(PhysicsUtils::createGLMTransform(glm::vec3(-1.318f, 2.408f, 0.765f), glm::vec3(-6.84f, 0.0f, -150.4f))), bottleHandModelMatrix(PhysicsUtils::createGLMTransform(glm::vec3(0.015f, 4.372f, 0.01f), glm::vec3(0, 90.0f, -180.0f))), useIndoorCamera(false)
 {
 	name = "Player Controller";
@@ -992,83 +1393,3 @@ void PlayerCharacter::imguiRender()
 	}
 }
 #endif
-
-
-//
-// ROPESIMULATION
-//
-void RopeSimulation::initializePoints(const std::vector<glm::vec3>& points)
-{
-	RopeSimulation::points = RopeSimulation::prevPoints = points;
-
-	size_t counter = 0;
-	for (size_t i = 0; i < points.size() - 1; i++)
-	{
-		distances.push_back(glm::length(points[i] - points[i + 1]));
-	}
-}
-
-void RopeSimulation::setPointPosition(size_t index, float trickleRate, const glm::vec3& position)
-{
-	glm::vec3 deltaMovement = position - points[index];
-	points[index] = position;
-
-	// NOTE: this is for alleviating glitchyness when point moves too much.
-	// This code propogates the movement from this new set operation to the other points of the rope
-	for (size_t i = 0; i < prevPoints.size(); i++)
-	{
-		prevPoints[i] += deltaMovement * glm::pow(trickleRate, (float)(i + 1));		// NOTE: I tried doing size-i, bc I thought that was more correct, but no, just doing i+1 for the exponent is correct -Timo 01-17-2022
-	}
-}
-
-void RopeSimulation::simulateRope(float gravityMultiplier)
-{
-	const float deltaTime = MainLoop::getInstance().deltaTime;
-
-	//
-	// Part 1: cycle thru all points (except #0 is locked, so skip that one), and update their positions
-	//
-	for (size_t i = 1; i < points.size(); i++)
-	{
-		glm::vec3 savedPoint = points[i];
-		points[i] += (points[i] - prevPoints[i]) + glm::vec3(0, -9.8f * gravityMultiplier * deltaTime * deltaTime, 0);		// TODO: figure out why the gravity term requires deltaTime * deltaTime instead of regular stuff huh
-		
-		// Limit to 45 degrees
-		if (limitTo45degrees)
-		{
-			const float sin45deg = 0.707106781f;
-			points[i].y = std::min(points[i].y, points[i - 1].y - distances[i - 1] * sin45deg);
-		}
-		
-		prevPoints[i] = savedPoint;
-	}
-
-	//
-	// Part 2: solve x times to try to get the distance correct again!
-	//
-	const int numIterations = 10;
-	for (int i = 0; i < numIterations; i++)
-	{
-		for (size_t j = 0; j < distances.size(); j++)
-		{
-			//if (glm::length(points[j] - points[j + 1]) < distances[j])
-			//	continue;
-
-			glm::vec3 midpoint = (points[j] + points[j + 1]) / 2.0f;
-			glm::vec3 direction = glm::normalize(points[j] - points[j + 1]);
-			if (j > 0)
-				points[j] = midpoint + direction * distances[j] / 2.0f;
-			points[j + 1] = midpoint - direction * distances[j] / 2.0f;
-		}
-		for (int j = (int)distances.size() - 1; j >= 0; j--)
-		{
-
-			// NOTE: ignore dumb warnings
-			glm::vec3 midpoint = (points[j] + points[j + 1]) / 2.0f;
-			glm::vec3 direction = glm::normalize(points[j] - points[j + 1]);
-			if (j > 0)
-				points[j] = midpoint + direction * distances[j] / 2.0f;
-			points[j + 1] = midpoint - direction * distances[j] / 2.0f;
-		}
-	}
-}
--- TestGUI/src/objects/PlayerCharacter.h
+++ TestGUI/src/objects/PlayerCharacter.h
@@ -5,8 +5,8 @@
 #include "../render_engine/model/animation/Animator.h"
 #include "../render_engine/camera/Camera.h"
 
-
 typedef unsigned int GLuint;
+
 
 //
 // TODO: place this class inside of somewhere!!!
@@ -28,6 +28,58 @@ private:
 	std::vector<glm::vec3> points;
 	std::vector<glm::vec3> prevPoints;
 	std::vector<float> distances;
+};
+
+
+class PlayerPhysics : public PhysicsComponent, public physx::PxUserControllerHitReport, public physx::PxControllerBehaviorCallback
+{
+public:
+	PlayerPhysics(BaseObject* bo);
+	~PlayerPhysics();
+
+	void lockVelocity(bool yAlso);
+
+	void physicsUpdate();
+	void propagateNewTransform(const glm::mat4& newTransform);
+	physx::PxTransform getGlobalPose();
+
+	physx::PxVec3 velocity;
+
+	physx::PxCapsuleController* controller;
+	physx::PxVec3 tempUp = physx::PxVec3(0.0f, 1.0f, 0.0f);
+
+	void setIsGrounded(bool flag) { isGrounded = flag; }
+	bool getIsGrounded() { return isGrounded; }
+	void setIsSliding(bool flag) { isSliding = flag; }
+	bool getIsSliding() { return isSliding; }
+	bool getStandingOnAngularVelocityY(float& dipstick) { dipstick = standingOnAngularVelocityYRadians; return hasValidStandingOnAngularVelocityY; }
+
+	glm::vec3 getGroundedNormal() { return groundHitNormal; }	// NOTE: this should not be accessed while !isGrounded, bc it isn't updated unless if on ground <45degrees
+
+	// PxUserControllerHitReport methods
+	virtual void onShapeHit(const physx::PxControllerShapeHit& hit);
+	virtual void onControllerHit(const physx::PxControllersHit& hit);
+	virtual void onObstacleHit(const physx::PxControllerObstacleHit& hit);
+
+	virtual physx::PxControllerBehaviorFlags getBehaviorFlags(const physx::PxShape& shape, const physx::PxActor& actor);
+	virtual physx::PxControllerBehaviorFlags getBehaviorFlags(const physx::PxController&);
+	virtual physx::PxControllerBehaviorFlags getBehaviorFlags(const physx::PxObstacle&);
+
+private:
+	bool isGrounded = false;
+	bool isSliding = false;
+	bool isSlidingCeiling = false;
+	bool isSandwiching = false;		// NOTE: this is where you push yourself into a crevice (defined by collision on bottom and top)
+	bool hasValidStandingOnAngularVelocityY = false;
+	float standingOnAngularVelocityYRadians;
+	glm::vec3 groundHitNormal = glm::vec3(0, 1, 0);
+	glm::vec3 ceilingHitNormal = glm::vec3(0, -1, 0);
+	physx::PxExtendedVec3 offsetFootMovedReconstructed;		// NOTE: this is only valid/updated when the hitnormal.y >= 0, so essentially if it's a bottom-hit
+	physx::PxExtendedVec3 offsetHeadMovedReconstructed;		// NOTE: this is only valid/updated when the hitnormal.y < 0, so essentially if it's a top-hit
+
+	// For retaining velocity when leaving the ground
+	bool prevIsGrounded = false;
+	physx::PxVec3 prevPositionWhileGrounded;
 };
 
 
